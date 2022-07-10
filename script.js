6a0c58b3674f38d80ce13123b5eb088391c1e4cd
Victor Mendes <victorutdsim7@gmail.com>
2022-07-10 12:01:50 -0300

    requisito 2

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const memeText = document.getElementById('meme-text');
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
 });
+
+const memeInsert = document.getElementById('meme-insert');
+memeInsert.addEventListener('input', () => {
+  const imagePath = URL.createObjectURL(memeInsert.files[0]);
+  const image = document.getElementById('meme-image');
+  image.src = imagePath;
+});
