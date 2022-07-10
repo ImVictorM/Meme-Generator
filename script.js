2f7c1554791df170606884a1162e93a3bab94947
Victor Mendes <victorutdsim7@gmail.com>
2022-07-10 12:44:51 -0300

    requisito 6

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ memeInsert.addEventListener('input', () => {
   const imageElement = document.getElementById('meme-image');
   imageElement.src = imagePath;
 });
+
+const styleButtons = document.getElementById('button-container').children;
+const memeImageContainer = document.getElementById('meme-image-container');
+
+function applyBorderStyle(element) {
+  const cssObj = window.getComputedStyle(element, null);
+  const bgColor = cssObj.getPropertyValue('background-color');
+  switch (element.id) {
+  case 'fire':
+    memeImageContainer.style.border = `3px dashed ${bgColor}`;
+    break;
+  case 'water':
+    memeImageContainer.style.border = `5px double ${bgColor}`;
+    break;
+  case 'earth':
+    memeImageContainer.style.border = `6px groove ${bgColor}`;
+    break;
+  default:
+    break;
+  }
+}
+
+for (let index = 0; index < styleButtons.length; index += 1) {
+  styleButtons[index].addEventListener('click', (event) => {
+    applyBorderStyle(event.target);
+  });
+}
