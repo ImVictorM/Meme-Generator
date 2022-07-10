0369365569ec87634b90cbba16843b3da0609d0c
Victor Mendes <victorutdsim7@gmail.com>
2022-07-10 11:07:10 -0300

    requisito 1

---
+++ script.js
@@ -0,0 +1,5 @@
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+textInput.addEventListener('keyup', () => {
+  memeText.innerText = textInput.value;
+});
